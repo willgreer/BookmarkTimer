282d55b5bc136371a6a7294f3c6357f35c78038d
William Greer <51033244+willgreer@users.noreply.github.com>
2020-03-07 12:53:33 -0700

    Add files via upload

---
+++ bookmark-reminder/content.js
@@ -0,0 +1,2 @@
+// content scripts are files that run in the context of web pages. by using the DOM they are able to read details and * pass thru details of the pages the browser visits/change/pass info TO their parent extension
+
